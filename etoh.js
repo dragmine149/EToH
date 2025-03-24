bc6e321b46124a3dc94cff624452f467c60463c4
dragmine <dan@thebanners.uk>
2025-03-24 21:38:55 +0000

    FInished adding tower badges to list, started work (massive) on making page actually function

--- etoh.js
+++ etoh.js
@@ -7,3 +7,93 @@
 
  * Somehow maybe? Get difficult from wiki or smth
 */
+let towers;
+
+function showError(message) {
+  document.getElementById('error_message').innerText = message;
+  document.getElementById('errors').hidden = false;
+}
+
+async function loadTowers() {
+  let server_towers = await fetch('tower_data.json');
+  if (!server_towers.ok) {
+    console.warn(server_towers);
+    showError(`Failed to fetch tower_data.json: ${server_towers.status} ${server_towers.statusText}.`);
+    return;
+  }
+
+  towers = await server_towers.json();
+  console.log(towers);
+}
+
+loadTowers();
+
+class Tower {
+  /** @type {string} */
+  name;
+  /** @type {number} */
+  difficulty;
+  /** @type {number} */
+  badge;
+
+  get shortName() {
+    return this.name.split(' ').map(word => word[0]).join('');
+  }
+  get difficultyWord() {
+    return getDifficulty(this.difficulty);
+  }
+
+  constructor(name, difficulty, badge) {
+    this.name = name;
+    this.difficulty = difficulty;
+    this.badge = badge;
+  }
+}
+
+/**
+* Translates the number form into a more readable word form
+* @param {number} difficulty The difficulty of the tower
+* @returns {string} The word form of the difficulty
+*/
+function getDifficulty(difficulty) {
+  let stage = Math.trunc(difficulty);
+  let sub = difficulty % 1;
+
+  let difficulties = ["Easy", "Medium", "Hard", "Difficult", "Challenging", "Intense", "Remorseless", "Insane", "Extreme", "Terrifying", "Catastrophic"];
+  let stageWord = difficulties[stage - 1] || "Unknown";
+
+  let subWord = ``;
+
+  if (sub >= 0.89) {
+    subWord = "Peak";
+  } else if (sub >= 0.78) {
+    subWord = "High-Peak";
+  } else if (sub >= 0.67) {
+    subWord = "High";
+  } else if (sub >= 0.56) {
+    subWord = "Mid-High";
+  } else if (sub >= 0.45) {
+    subWord = "Mid";
+  } else if (sub >= 0.33) {
+    subWord = "Low-Mid";
+  } else if (sub >= 0.22) {
+    subWord = "Low";
+  } else if (sub >= 0.11) {
+    subWord = "Bottom-Low";
+  } else {
+    subWord = "Bottom";
+  }
+
+
+  return `${stageWord} ${subWord}`;
+}
+
+
+class UserManager {
+  /** @type {number} Roblox UserID */
+  user;
+
+  async loadUserData() {
+
+  }
+}
