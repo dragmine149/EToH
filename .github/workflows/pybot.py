16e10442a2e56dcdb0e32680e45dddd66f1baa80
Dragmine149 <dan@thebanners.uk>
2026-01-29 01:21:00 +0000

    get pybot

--- .github/workflows/pybot.py
+++ .github/workflows/pybot.py
@@ -2,4 +2,4 @@ import pywikibot
 
 for i in range(1000):
     site = pywikibot.Site(url="https://jtoh.fandom.com/api.php")
-    pywikibot.Page(site, "TOAST")
+    pywikibot.Page(site, "TOAST").get(True, True)
